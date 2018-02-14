eb114c45d5c17de7955ad16fe7a2afc70a9e49f9
Junyuan Hong <jyhong836@gmail.com>
2018-02-14 18:03:55 +0800

    Extract the model factory into new file.

---
+++ fbmodels.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import keras.backend
+
+class ModelFactory():
+    """My model factories
+    """
+
+    @staticmethod
+    def naive_dqn(state_size, action_size, learning_rate):
+        """ Neural Net for Deep-Q learning Model
+        """
+        print('Build Deep Q-learnig Model.')
+
+        def huber_loss(target, prediction):
+            """sqrt(1+error^2)-1"""
+            return keras.backend.mean(keras.backend.sqrt(1+keras.backend.square(prediction - target))-1, axis=-1)
+
+        model = Sequential()
+        # print('### '+repr(state_size))
+        model.add(Dense(24, input_shape=state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(action_size, activation='linear'))
+        model.compile(loss=huber_loss,
+                      optimizer=Adam(lr=learning_rate))
+        return model
--- flappybird_agent.py
+++ flappybird_agent.py
@@ -3,35 +3,8 @@ import numpy as np
 from collections import deque
 from ple.games.flappybird import FlappyBird
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-import keras.backend
-
 from fb_trainer import Trainer
-
-class MyModelFact():
-    """My model factories
-    """
-    
-    @staticmethod
-    def naive_dqn(state_size, action_size, learning_rate):
-        """ Neural Net for Deep-Q learning Model
-        """
-        print('Build Deep Q-learnig Model.')
-
-        def huber_loss(target, prediction):
-            """sqrt(1+error^2)-1"""
-            return keras.backend.mean(keras.backend.sqrt(1+keras.backend.square(prediction - target))-1, axis=-1)
-
-        model = Sequential()
-        # print('### '+repr(state_size))
-        model.add(Dense(24, input_shape=state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(action_size, activation='linear'))
-        model.compile(loss=huber_loss,
-                      optimizer=Adam(lr=learning_rate))
-        return model
+from fbmodels import ModelFactory
 
 class AgentType(object):
     def __init__(self, state_size, action_space):
@@ -68,7 +41,8 @@ class MyAgent(AgentType):
         self.model = self._build_model()
     
     def _build_model(self):
-        model = MyModelFact.naive_dqn(self.state_size, len(self.action_space), self.learning_rate)
+        model = ModelFactory.naive_dqn(self.state_size, len(
+            self.action_space), self.learning_rate)
         return model
 
     def act(self, reward, state):
@@ -86,6 +60,7 @@ class MyAgent(AgentType):
             return # not study
 
         def do_with_play(curstate, action, reward, next_state, game_over):
+            # print(curstate, action, reward, next_state, game_over)
             target = self.model.predict(curstate)
             if game_over:
                 target[0][action] = reward
@@ -93,7 +68,7 @@ class MyAgent(AgentType):
                 # TODO replace the 'self.model' with a stable one.
                 target[0][action] = reward + self.gamma * \
                     np.amax(self.model.predict(next_state)[0])
-            self.model.fit(curstate, target, epochs=1, verbose=0) # TODO use [curstate, action] as input
+            self.model.fit(curstate, target, epochs=1, verbose=0)
             return None
 
         self._replay(self.batch_size, do_with_play)
