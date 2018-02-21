a2f04475aaaf87bc42e1d6a5eaad0c730506c1a2
Junyuan Hong <jyhong836@gmail.com>
2018-02-21 10:58:02 +0800

    Log train/test

--- fb_logger.py
+++ fb_logger.py
@@ -15,14 +15,18 @@ class train_logger(object):
             reward = tf.Variable(0.0, name="reward", trainable=False)
             loss = tf.Variable(0.0, name="loss", trainable=False)
             # create summaries
-            tf.summary.scalar("reward", reward)
-            tf.summary.scalar("loss", loss)
+            reward_sum = tf.summary.scalar("reward", reward)
+            loss_sum = tf.summary.scalar("loss", loss)
+            # tf.summary.scalar('epsilon', )
         with tf.name_scope("test"):
             # create variables
             te_reward = tf.Variable(0.0, name="reward", trainable=False)
             # create summaries
-            tf.summary.scalar("reward", te_reward)
-        return reward, loss, te_reward
+            ter_sum = tf.summary.scalar("reward", te_reward)
+        # merge op
+        summary_op = tf.summary.merge([reward_sum, loss_sum])
+        te_summary_op = tf.summary.merge([ter_sum])
+        return reward, loss, te_reward, summary_op, te_summary_op
 
 
     def __init__(self, summary_dir):
@@ -30,9 +34,7 @@ class train_logger(object):
 
         # a simple session
         sess = tf.Session()
-        self.reward, self.loss, self.te_reward = self._create_train_variables()
-        # merge op
-        summary_op = tf.summary.merge_all() 
+        self.reward, self.loss, self.te_reward, summary_op, te_summary_op = self._create_train_variables()
         # create writer
         summary_writer = tf.summary.FileWriter(self.summary_dir, sess.graph)
 
@@ -41,21 +43,24 @@ class train_logger(object):
 
         self.sess = sess
         self.summary_op = summary_op
+        self.te_summary_op = te_summary_op
         self.summary_writer = summary_writer
 
-    def log(self, step, reward, loss, te_reward):
+    def log(self, step, reward, loss, is_test=False):
         """Log data by one step.
         """
-        feed_dict = {
-            self.reward   : reward,
-            self.loss     : loss,
-            self.te_reward: te_reward,
-        }
-
-        # sess.run returns a list, so we have to explicitly
-        # extract the first item using sess.run(...)[0]
-        summaries = self.sess.run([self.summary_op], feed_dict)[0]
-        self.summary_writer.add_summary(summaries, step)
+        if is_test:
+            feed_dict = {
+                self.te_reward: reward
+            }
+            self.summary_writer.add_summary(self.sess.run([self.te_summary_op], feed_dict)[0], step)
+        else:
+            feed_dict = {
+                self.reward: reward,
+                self.loss: loss,
+            }
+            self.summary_writer.add_summary(self.sess.run(
+                [self.summary_op], feed_dict)[0], step)
     
     def close(self): # FIXME not closed
         self.summary_writer.close()
@@ -63,10 +68,12 @@ class train_logger(object):
 if __name__ == "__main__": # for test
     logger = train_logger('save')  # logdir='save'
 
-    fake_info = rand(100, 3) # random data
+    fake_info = rand(100, 2) # random data
 
     for step, infoset in enumerate(fake_info):
-        logger.log(step, infoset[0], infoset[1], infoset[2])
+        if step % 10 == 0:
+            logger.log(step, infoset[0], infoset[1], is_test=True)
+        logger.log(step, infoset[0], infoset[1], is_test=False)
 
     logger.close()
 
