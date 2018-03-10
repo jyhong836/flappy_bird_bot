f5b0e6f4f6fdb5c5e430bd208032ff836907e4db
Junyuan Hong <jyhong836@gmail.com>
2018-03-10 09:25:35 +0800

    FIX: GPU limit

--- fbmodels.py
+++ fbmodels.py
@@ -1,24 +1,32 @@
+import keras.backend
+
+# print("------- backend: ", keras.backend.backend())
+# if "tensorflow" == keras.backend.backend():
+#     from tensorflow.python.client import device_lib
+#     print("--- type: ", device_lib.list_local_devices())
+#     for dv in device_lib.list_local_devices():
+#         dt = dv.device_type
+#         if "GPU" == dt:
+#             print("GPU setting.....")
+#             import tensorflow as tf
+#             from keras.backend.tensorflow_backend import set_session
+#             config = tf.ConfigProto()
+#             config.gpu_options.allow_growth = True
+#             # config.gpu_options.visible_device_list = "0"
+#             set_session(tf.Session(config=config))
+#             break
+
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+# config.gpu_options.visible_device_list = "0"
+set_session(tf.Session(config=config))
+
+# import sys
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-import keras.backend
-# import sys
-
-print("------- backend: ", keras.backend.backend())
-if "tensorflow" == keras.backend.backend():
-    from tensorflow.python.client import device_lib
-    print("--- type: ", device_lib.list_local_devices())
-    for dv in device_lib.list_local_devices():
-        dt = dv.device_type
-        if "GPU" == dt:
-            print("GPU setting.....")
-            import tensorflow as tf
-            from keras.backend.tensorflow_backend import set_session
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            # config.gpu_options.visible_device_list = "0"
-            set_session(tf.Session(config=config))
-            break
 
 # sys.exit()
 
